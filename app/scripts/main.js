1b1827597a694ac7d057c19d3d8b662d4c39d1ed
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2016-03-15 16:05:47 -0700

    hook in jsond

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,10 @@
-/*global OpenSeadragon, $, parseUri */
+/*global OpenSeadragon, $, parseUri, populateMetadata */
 'use strict';
+// create a fake YUI
+var YAHOO = {}; YAHOO.util = {}; YAHOO.util.Dom = document;
+YAHOO.util.Dom.get = document.getElementById;
 
+// refactor the zark (z-indexed ARK) functions to mimimize .split?
 function zarkToKey(zark) {
   var park = zark.split('/');
   return park[1] + '-' + park[2] + '-' + park[3] + '.jp2';
@@ -12,6 +16,19 @@ function zarkToFrame(zark) {
   url = url + '/?order=' + park[3].slice(1) + ';layout=iframe';
   return url;
 }
+
+function injectScript(zark) {
+  var park = zark.split('/');
+  var url = 'http://content.cdlib.org/ark:/' + park[1] + '/' + park[2] + '/?mode=jsod';
+  var script = document.createElement('script');
+  script.onload = function(){  // http://stackoverflow.com/a/8578723/1763984
+    script.onload = null;
+    populateMetadata();
+  };
+  $(script).appendTo('#metadata');
+  script.src = url;
+}
+
 
 $(document).ready(function() {
   var zark = parseUri(window.location).queryKey.ark;
@@ -48,4 +65,5 @@ $(document).ready(function() {
     console.log('complete');
     // TODO -- log stuff to google analytics
   });
+  injectScript(zark);
 });
